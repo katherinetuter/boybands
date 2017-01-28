f35344703a3259afff8463c8ec3703ba2a447fdf
katherine tuter <katherinetuter@gmail.com>
2017-01-27 20:49:20 -0600

    figuring out the details of the loop

--- bands.js
+++ bands.js
@@ -2,23 +2,16 @@
 var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
 var vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
 var loopCount = 5;
+var currentBand = 0;
+var currentVeggie = 0;
 
 //start loop 
 for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
-	var currentBand = bands[loopTracker];
-	var currentVeggie = vegetables[loopTracker];
 	var bandElement = document.getElementById("boy-bands");
-	bandElement.innerHTML = currentBand;
+	console.log(currentBand);
+	bandElement.innerHTML = "<p>" + bands[loopTracker] + "</p>";
 	var veggieElement = document.getElementById("vegetables");
+	veggieElement.innerHTML = "<p>" + vegetables[loopTracker] + "</p>";
 
 }
 
-
-//   // Add the band names into the correct <div>
-//   currentBand = ???;
-
-
-//   // Add the veggie names into the correct <div>
-//   currentVeggie = ???;
-
-// }
